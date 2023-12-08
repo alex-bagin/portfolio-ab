91000d70d8fd900e3eaddf4ed3373ea0d40579e5
Alex Bagin <alex.bagin@gmx.de>
2023-12-08 16:45:41 +0100

    modified gitignore and add readme

--- src/context/index.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-export const ThemeContext = createContext(null);
-export const DarkContext = createContext(null);
-export const AuthContext = createContext(null);
-export const MenuContext = createContext(null);
