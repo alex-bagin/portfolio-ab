a8f17ece8be7cf97d76b6025067ee2553802ceeb
Alex Bagin <alex.bagin@gmx.de>
2023-12-16 12:34:34 +0100

    modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  publicDir: "https://alex-bagin.github.io/portfolio-ab/",
+  root: "https://alex-bagin.github.io/portfolio-ab/",
   plugins: [react()],
 });
