c8102eedde064360c3c009cd4cb1a8fdaa635f37
Alex Bagin <alex.bagin@gmx.de>
2023-12-18 20:19:12 +0100

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/portfolio-ab/",
+  publicDir: "/portfolio-ab",
   plugins: [react()],
   build: { chunkSizeWarningLimit: 600 },
 });
