2df95b738f2c3484c474301f20d9f81ec90b9b23
Alex Bagin <alex.bagin@gmx.de>
2023-12-18 21:00:47 +0100

    fix hero section

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/portfolio-ab/",
+  publicDir: true,
   plugins: [react()],
   build: { chunkSizeWarningLimit: 600 },
 });
